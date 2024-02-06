4b9b22b9d4963146cf701e3de645afbe1a3dabdf
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2024-02-06 09:40:42 +0100

    Also install package if not available in repo

--- addMCRApackages.R
+++ addMCRApackages.R
@@ -12,7 +12,9 @@ pkgDeps <- sapply(X = pkgList,
                   ignores = "", lib.loc = .libPaths())
 
 pkgInstFull <- unique(c(unlist(pkgDeps), pkgList))
-pkgInstFull <- setdiff(pkgInstFull, "proast70.3")
+## proast and opex are not available on CRAN and have to be installed by hand.
+## Exclude them from the list.
+pkgInstFull <- setdiff(pkgInstFull, c("proast70.3", "opex"))
 
 pkgInstDat <- as.data.frame(pkgInst[rownames(pkgInst) %in% pkgInstFull &
                                       is.na(pkgInst[, "Priority"]), ])
@@ -46,13 +48,14 @@ for (Rver in c("4.2", "4.3")) {
     pkgName <- pkgInstDat$Package[i]
     pkgVer <- pkgInstDat$Version[i]
     instVersion <- pkgInstMCRA[pkgName, "Version"]
-    if (pkgVer > instVersion) {
+    if (is.na(instVersion) || pkgVer > instVersion) {
       pkgString <- paste0(pkgName, "_", pkgVer, ".zip")
       download.file(paste0("https://cran.r-project.org/bin/windows/contrib/", 
                            Rver, "/", pkgString),
                     destfile = file.path(tmpDir, pkgString))
       insertPackage(file = file.path(tmpDir, pkgString),
                     repodir = paste0("."),
-                    commit = TRUE)    }
+                    commit = TRUE)    
+    }
   }
 }
