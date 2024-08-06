e301d5c05864297644ad17adf5fd027b2e785249
Lenthe, Marco van <marco.vanlenthe@wur.nl>
2024-08-06 16:07:32 +0200

    Update script for R4.4 and add Proast 71.1 link, copy opex to 4.4 folder

--- addMCRApackages.R
+++ addMCRApackages.R
@@ -1,60 +1,65 @@
 library(drat)
 
+## Configuration
+## Production values:
+CommitPackagesToRepo <- TRUE
+RemoteWriteRepo <- 'https://biometris.github.io/MCRARpackages'
+## Test values:
+# CommitPackagesToRepo <- FALSE
+# RemoteWriteRepo <- 'e:/Git/LocalMCRAPackages.git'
+
+RemoteDownloadRepo <- 'https://cran.r-project.org'
+
 ## Get all installed packages.
 pkgInst <- installed.packages()
 
 ## Load file with packages used in MCRA.
-pkgList <- read.delim("./rpackages.txt", header = FALSE)$V1 
+pkgList <- read.delim("./rpackages.txt", header = FALSE)$V1
 
 ## Get all dependencies.
 pkgDeps <- sapply(X = pkgList,
-                  FUN = packrat:::recursivePackageDependencies, 
+                  FUN = packrat:::recursivePackageDependencies,
                   ignores = "", lib.loc = .libPaths())
 
 pkgInstFull <- unique(c(unlist(pkgDeps), pkgList))
 ## proast and opex are not available on CRAN and have to be installed by hand.
 ## Exclude them from the list.
-pkgInstFull <- setdiff(pkgInstFull, c("proast70.3", "opex"))
+pkgInstFull <- setdiff(pkgInstFull, c("proast71.1", "opex"))
 
 pkgInstDat <- as.data.frame(pkgInst[rownames(pkgInst) %in% pkgInstFull &
                                       is.na(pkgInst[, "Priority"]), ])
 
-## Add to github repo for R versions 4.2 and 4.3
+## Add to github repo for R versions 4.2 and 4.3 and 4.4
 
 options(dratBranch = "docs")
 
 tmpDir <- tempdir()
 
 ## Add proast to repo.
-## proast is compiled for R 4.0. 
+## proast is compiled for R 4.3.2.
 
-download.file("https://www.rivm.nl/sites/default/files/2021-06/proast70.3.zip",
-              destfile = file.path(tmpDir, "proast70.3.zip"))
+download.file("https://www.rivm.nl/sites/default/files/2024-04/proast71.1.zip",
+              destfile = file.path(tmpDir, "proast71.1.zip"))
 
-file.rename(file.path(tmpDir, "proast70.3.zip"), 
-            file.path(tmpDir, "proast70.3_0.01.zip"))
+file.rename(file.path(tmpDir, "proast71.1.zip"),
+            file.path(tmpDir, "proast71.1_0.01.zip"))
 
-insertPackage(file = file.path(tmpDir, "proast70.3_0.01.zip"),
+insertPackage(file = file.path(tmpDir, "proast71.1_0.01.zip"),
               repodir = paste0("."),
-              commit = TRUE)
+              commit = CommitPackagesToRepo)
 
 ## Add opex to repo.
 ## For opex the source code is available. From this we compiled binaries for
-## R.4.2 and R.4.3. These are added to the repo.
-insertPackage(file = file.path("./opex/4.2", "opex_2.0.0.zip"),
+## R4.4, (previously R.4.2 and R.4.3.) These are added to the repo.
+insertPackage(file = file.path("./opex/4.4", "opex_2.0.0.zip"),
               repodir = paste0("."),
-              commit = TRUE)
-
-insertPackage(file = file.path("./opex/4.3", "opex_2.0.0.zip"),
-              repodir = paste0("."),
-              commit = TRUE)
-
+              commit = CommitPackagesToRepo)
 
 ## Add all other packages to github repo.
-## Packages are added for R versions 4.2.x and 4.3.x.
+## Packages are added for R versions 4.4.x. (previously 4.2.x and 4.3.x)
 ## Only packages for which a newer version is available are added.
-for (Rver in c("4.2", "4.3")) {
-  repoLoc <- paste0("https://biometris.github.io/MCRARpackages/bin/windows/contrib/", Rver)
+for (Rver in c("4.4")) {
+  repoLoc <- paste0(RemoteWriteRepo, "/bin/windows/contrib/", Rver)
   pkgInstMCRA <- as.data.frame(available.packages(repoLoc))
   for (i in 1:nrow(pkgInstDat)) {
     pkgName <- pkgInstDat$Package[i]
@@ -62,12 +67,12 @@ for (Rver in c("4.2", "4.3")) {
     instVersion <- pkgInstMCRA[pkgName, "Version"]
     if (is.na(instVersion) || pkgVer > instVersion) {
       pkgString <- paste0(pkgName, "_", pkgVer, ".zip")
-      download.file(paste0("https://cran.r-project.org/bin/windows/contrib/", 
+      download.file(paste0(RemoteDownloadRepo, "/bin/windows/contrib/",
                            Rver, "/", pkgString),
                     destfile = file.path(tmpDir, pkgString))
       insertPackage(file = file.path(tmpDir, pkgString),
                     repodir = paste0("."),
-                    commit = TRUE)    
+                    commit = CommitPackagesToRepo)
     }
   }
 }
